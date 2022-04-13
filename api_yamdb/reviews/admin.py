b3b9dbb4f4760a003bbffc53f88bc6717c89865b
Кирилл Косоротов <ampolirostinvest@yandex.ru>
2022-04-13 09:12:39 +0300

    добавил в admin группы

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Category, Genre, Title
 
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Title)
