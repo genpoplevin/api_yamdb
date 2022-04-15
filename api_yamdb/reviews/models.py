c164bcd13987ce3e8ae798c0838cc963c7177ea8
Кирилл Косоротов <ampolirostinvest@yandex.ru>
2022-04-15 12:33:13 +0300

    sql

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -59,9 +59,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = (
-            'username',
-        )
+        ordering = ['username']
 
     def __str__(self):
         return self.username
