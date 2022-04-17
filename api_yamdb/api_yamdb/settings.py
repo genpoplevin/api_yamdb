80263dc73383847d8a0bc51bb1548fb70aa8b32c
Кирилл Косоротов <ampolirostinvest@yandex.ru>
2022-04-17 19:20:59 +0300

    3 заход

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,8 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'api.apps.ApiConfig',
-    'reviews.apps.ReviewsConfig',
+    'api',
+    'reviews',
 ]
 
 MIDDLEWARE = [
